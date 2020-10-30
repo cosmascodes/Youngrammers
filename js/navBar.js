d45f8acb56b4bbe2f7359738085d58f5f29a5ef7
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2020-10-30 13:24:34 +0300

    updated

--- js/navBar.js
+++ js/navBar.js
@@ -1,3 +1,13 @@
-document.querySelector('#nav-bar').addEventListener('click',()=>{
-    document.getElementById('nav').classList.toggle('on')
+let navBar = document.querySelector('#nav-bar')
+let navCont = document.getElementById('nav')
+let links = document.querySelectorAll('#link')
+
+navBar.addEventListener('click', e => {
+    navCont.classList.toggle('on')
+})
+
+links.forEach( link => {
+    link.addEventListener('click',()=>{
+        navCont.classList.toggle('on')
+    })
 })
